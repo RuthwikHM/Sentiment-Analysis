c6d0c7003aeb0d4143ae69239b88451e8ffb0f7e
RuthwikHM <41834689+RuthwikHM@users.noreply.github.com>
2018-09-28 19:20:18 +0530

    Updated code
    Added dropout to increase evalaution accuracy to 77.7%

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -2,7 +2,7 @@
 import keras
 import numpy as np
 import pandas as pd
-from keras.layers import LSTM,Embedding
+from keras.layers import LSTM,Embedding,Dropout
 from keras.models import Sequential
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
@@ -17,10 +17,15 @@ x_train=tokenizer.texts_to_sequences(data['text'].values)
 x_train=sequence.pad_sequences(x_train)
 y_train=pd.get_dummies(data['sentiment'])
 x_train,x_test,y_train,y_test=train_test_split(x_train,y_train,test_size=0.3)
+#Creating the model
 model=Sequential()
 model.add(Embedding(2000,128,input_length=29))
 model.add(LSTM(3))
+model.add(Dropout(0.2))
+#Compiling the model
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+#Fiitng data to model
 model.fit(x_train,y_train,epochs=20,batch_size=280,validation_split=0.3,verbose=2)
+#Evaluating the model
 score=model.evaluate(x_test,y_test,verbose=2,batch_size=280)
 print('Evaluation accuracy=',(score[1]*100))
